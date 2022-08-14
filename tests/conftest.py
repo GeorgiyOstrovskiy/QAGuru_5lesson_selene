fe616f146d031b81d88ea52efa8b8c9c7bee845d
xacker <egor_kinderr@mail.ru>
2022-08-14 12:50:31 +0300

    add check practiceForm

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,4 @@ import pytest
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.browser_name = 'chrome'
--- tests/test_demoqa_practiceForm.py
+++ tests/test_demoqa_practiceForm.py
@@ -1,8 +1,36 @@
+from os.path import abspath
 from selene.support.shared import browser
-from selene import have, be
+from selene import have, command
 
 
 def test_practice_form():
     browser.open('/automation-practice-form')
-    browser.should(have.title('ToolsQA'))
+    browser.element('#firstName').type('Egor')
+    browser.element('#lastName').type('Egorov')
+    browser.element('#userEmail').type('egor@oz.ru')
+    browser.all('.custom-radio').element_by(have.exact_text('Female')).click()
+    browser.element('#userNumber').type('1111111111')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').element(f'[value="{1999}"]').click()
+    browser.element('.react-datepicker__month-select').element(f'[value="{5}"]').click()
+    browser.element('.react-datepicker__day--003').click()
+    browser.element('#subjectsInput').type('Physics').press_enter()
+    browser.element('#hobbiesWrapper').all('.custom-checkbox').element_by(have.exact_text('Sports')).click()
+    browser.element('#uploadPicture').send_keys(abspath('../resources/test_image.png'))
+    browser.element('#currentAddress').type('Ukraine')
+    browser.element('#state').element('input').type('Haryana').press_enter()
+    browser.element('#city').element('input').type('Karnal').press_enter()
+    browser.element('#submit').perform(command.js.click)
+
+    browser.all(".modal-dialog").all("table tr")[1].all("td").should(have.exact_texts('Student Name', 'Egor Egorov'))
+    browser.all(".modal-dialog").all("table tr")[2].all("td").should(have.exact_texts('Student Email', 'egor@oz.ru'))
+    browser.all(".modal-dialog").all("table tr")[3].all("td").should(have.exact_texts('Gender', 'Female'))
+    browser.all(".modal-dialog").all("table tr")[4].all("td").should(have.exact_texts('Mobile', '1111111111'))
+    browser.all(".modal-dialog").all("table tr")[5].all("td").should(have.exact_texts('Date of Birth', '03 June,1999'))
+    browser.all(".modal-dialog").all("table tr")[6].all("td").should(have.exact_texts('Subjects', 'Physics'))
+    browser.all(".modal-dialog").all("table tr")[7].all("td").should(have.exact_texts('Hobbies', 'Sports'))
+    browser.all(".modal-dialog").all("table tr")[8].all("td").should(have.exact_texts('Picture', 'test_image.png'))
+    browser.all(".modal-dialog").all("table tr")[9].all("td").should(have.exact_texts('Address', 'Ukraine'))
+    browser.all(".modal-dialog").all("table tr")[10].all("td").should(have.exact_texts('State and City',
+                                                                                       'Haryana Karnal'))
 
